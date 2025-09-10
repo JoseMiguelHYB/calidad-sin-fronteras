705b708f98a572ad8e2dae801187cd70d88643c3
José Miguel Castro Huayllas <JoseMiguelHYB@gmail.com>
2025-09-10 21:41:46 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -199,3 +199,25 @@ closeBtn.addEventListener("click", () => {
       }
     });
   }
+
+
+  // carrusel de portada
+  const slides = document.querySelectorAll('.carousel-slide');
+  let current = 0;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.remove('active');
+      if (i === index) {
+        slide.classList.add('active');
+      }
+    });
+  }
+
+  function nextSlide() {
+    current = (current + 1) % slides.length;
+    showSlide(current);
+  }
+
+  // Cambia cada 7 segundos
+  setInterval(nextSlide, 7000);
